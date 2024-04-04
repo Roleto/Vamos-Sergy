04054d78193f486963f3d72f6a510c2d0ae9cebf
Roland Gulya <mgulyaroland@gmail.com>
2024-04-04 12:50:26 +0200

    Add project files.

---
+++ Vamos&Sergy/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Vamos_Sergy.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
